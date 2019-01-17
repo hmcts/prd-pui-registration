4d959d9b5c55c4029ae049239071f223a3be35eb
fazanki <fazanki@gmail.com>
2019-01-17 15:06:02 +0000

    adding styles and header component from jui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './containers/app/app.component';
+import {HeaderComponent} from './containers/header/header.component';
+import {HmctsGlobalHeaderComponent} from './components/hmcts-global-header/hmcts-global-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HmctsGlobalHeaderComponent
   ],
   imports: [
     BrowserModule
