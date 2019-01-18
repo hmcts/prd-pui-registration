8bcb1bd78142202be4bbce3995b6f407e53592c5
fazanki <fazanki@gmail.com>
2019-01-18 13:45:22 +0000

    adding registration module/page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './containers/app/app.component';
-import {HeaderComponent} from './containers/header/header.component';
-import {HmctsGlobalHeaderComponent} from './components/hmcts-global-header/hmcts-global-header.component';
-
 import {RegisterModule} from '../register/register.module';
+
+// from Containers
+import * as fromContainers from './containers/';
+
+// from Components
+import * as fromComponents from './components';
 
 export const ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'register' }
@@ -15,8 +18,8 @@ export const ROUTES: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HmctsGlobalHeaderComponent
+    ...fromComponents.components,
+    ...fromContainers.containers
   ],
   imports: [
     BrowserModule,
---
+++ src/app/containers/index.ts
@@ -0,0 +1,10 @@
+
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+
+export const containers: any[] = [
+  HeaderComponent,
+  FooterComponent
+];
+
+export * from '../components/hmcts-global-header/hmcts-global-header.component';
