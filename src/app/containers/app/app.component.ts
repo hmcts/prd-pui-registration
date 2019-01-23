7b29f09b1e76762dd70fb8dda253776ad9ba693d
fazanki <fazanki@gmail.com>
2019-01-23 14:50:41 +0000

    fixing the page layout and adding the back button

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import * as fromRoot from '../../store';
+import {Store} from '@ngrx/store';
+import * as fromStore from '../../../register/store';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private store: Store<fromRoot.State>) {}
+
+  onGoBack() {
+    this.store.dispatch(new fromRoot.Back())
+  }
 }
