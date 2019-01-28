bb952a270c2de37639d4ac184baa83213a99ea62
Chris Tarasovs <chris@frontr.co.uk>
2019-01-28 12:51:10 +0000

    FixTest: Introducing NO_ERRORS_SCHEMA

--- src/app/components/hidden-input/hidden-input.component.spec.ts
+++ src/app/components/hidden-input/hidden-input.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {InputsComponent} from '../inputs/inputs.component';
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
 
 
 
@@ -9,7 +10,8 @@ describe('InputsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InputsComponent ]
+      declarations: [ InputsComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
   }));
