8b530cce0b6711f5c267cee0b7c3a7c62bb35c72
Kristaps Tarasovs <31708534+frontr-uk@users.noreply.github.com>
2019-01-30 11:44:47 +0000

    Test:Introducing test to ngrx store (#13)
    * test
    
    * Test: Introducing test to store

--- src/register/store/effects/registration.effects.ts
+++ src/register/store/effects/registration.effects.ts
@@ -19,7 +19,10 @@ export class RegistrationEffects {
     map((action: registrationActions.LoadPageItems) => action.payload),
     switchMap((pageId) => {
       return this.registrationService.getRetistrationFrom(pageId).pipe(
-        map(returnedItems => new registrationActions.LoadPageItemsSuccess({payload: returnedItems, pageId})),
+        map(returnedItems => {
+          return new registrationActions.LoadPageItemsSuccess({payload: returnedItems, pageId})
+
+        }),
         catchError(error => of(new registrationActions.LoadPageItemsSuccess(error)))
       );
     })
