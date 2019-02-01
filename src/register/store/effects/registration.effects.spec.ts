2a9339933caf7e643971dc2ad37b834ab81ecf4f
Chris Tarasovs <chris@frontr.co.uk>
2019-02-01 13:54:59 +0000

    Effects

--- src/register/store/effects/registration.effects.spec.ts
+++ src/register/store/effects/registration.effects.spec.ts
@@ -3,40 +3,26 @@ import { TestBed } from '@angular/core/testing';
 
 import { Actions } from '@ngrx/effects';
 import { hot, cold } from 'jasmine-marbles';
-import { Observable } from 'rxjs';
-// import { empty } from 'rxjs/observable/empty';
+import {EMPTY, Observable} from 'rxjs';
 import { of } from 'rxjs';
 import { provideMockActions } from '@ngrx/effects/testing';
 import * as fromRegistrationEffects from './registration.effects';
 import {RegistrationFormService} from '../../services/registration-form.service';
 import {RegistrationEffects} from './registration.effects';
 import {LoadPageItems} from '../actions/registration.actions';
-import {LoadPageItemsSuccess} from '../actions';
+import {LoadPageItemsFail, LoadPageItemsSuccess} from '../actions';
+import {RouterTestingModule} from '@angular/router/testing';
 
-// export class TestActions extends Actions {
-//   constructor() {
-//     // super(empty());
-//   }
-//
-//   // set stream(source: Observable<any>) {
-//   //   this.source = source;
-//   // }
-// }
 
-// export function getActions() {
-//   return new TestActions();
-// }
 
 
 describe('Registration Effects', () => {
-  let actions$;
+  let actions$: Observable<any>;
   let effects: RegistrationEffects;
-  const RegistrationFormServiceMock = jasmine.createSpyObj('RegistrationFormService', [
-    'getRetistrationFrom',
-  ]);
+  const RegistrationFormServiceMock = jasmine.createSpyObj('RegistrationFormService', ['getRetistrationFrom']);
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
       providers: [
           {
             provide: RegistrationFormService,
@@ -44,7 +30,9 @@ describe('Registration Effects', () => {
           },
           fromRegistrationEffects.RegistrationEffects,
           provideMockActions(() => actions$)
-          // { provice; Actions, useFactory: getActions }
+          // {
+          //   provice: Actions,
+          //   useFactory: getActions }
       ]
     });
 
@@ -53,7 +41,7 @@ describe('Registration Effects', () => {
 
   });
   describe('loadRegistrationForm$', () => {
-    it('should return a collection from loadRegistrationForm$ - LoadPageItemsSuccess', () => {
+    it('should return a loadRegistrationForm$, with data, on LoadPageItemsSuccess', () => {
       const pageId = 'something';
       RegistrationFormServiceMock.getRetistrationFrom.and.returnValue(of(pageId));
       const action = new LoadPageItems(pageId);
@@ -62,6 +50,24 @@ describe('Registration Effects', () => {
       const expected = cold('-b', { b: completion });
       expect(effects.loadRegistrationForm$).toBeObservable(expected);
     });
+    it('shoud return a LoadPageItemsFail there is a falure', () => {
+
+        const error = 'Service HTTP failure';
+        const action = new LoadPageItems('pagename');
+        const completion = new LoadPageItemsFail(error);
+
+          // actions$.stream = hot('-a', { a: action});
+        const responce = cold('-#', {}, error);
+      const expected = cold('--b', { b: completion });
+      RegistrationFormServiceMock.getRetistrationFrom.and.returnValue(responce);
+      expect(RegistrationFormServiceMock.getRetistrationFrom).toHaveBeenCalled();
+         // .andReturn(responce);
+      // expected(effects.loadRegistrationForm$).toBeObservable(actions)
+      // console.log('d', effects.loadRegistrationForm$)
+      // expected(effects.loadRegistrationForm$).toBeObservable(new LoadPageItemsFail(error));
+
+    })
+
   });
 
 
--- src/register/store/effects/registration.effects.ts
+++ src/register/store/effects/registration.effects.ts
@@ -3,8 +3,10 @@ import {Actions, Effect, ofType} from '@ngrx/effects';
 
 import * as registrationActions from '../actions';
 import {catchError, map, switchMap} from 'rxjs/operators';
-import {of} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {RegistrationFormService} from '../../services/registration-form.service';
+import {LoadPageItems} from '../actions';
+import {Action} from '@ngrx/store';
 
 @Injectable()
 export class RegistrationEffects {
@@ -14,15 +16,14 @@ export class RegistrationEffects {
   ) {}
 
   @Effect()
-  loadRegistrationForm$ = this.actions$.pipe(
-    ofType(registrationActions.LOAD_PAGE_ITEMS),
+  loadRegistrationForm$: Observable<Action> = this.actions$.pipe(
+    ofType<LoadPageItems>(registrationActions.LOAD_PAGE_ITEMS),
     map((action: registrationActions.LoadPageItems) => action.payload),
-    switchMap((pageId) => {
+    switchMap((pageId: string) => {
       return this.registrationService.getRetistrationFrom(pageId).pipe(
         map(returnedItems => {
           // returnedItems to be passed as Object otherwise interactes each charater
           return new registrationActions.LoadPageItemsSuccess({payload: returnedItems, pageId});
-
         }),
         catchError(error => of(new registrationActions.LoadPageItemsSuccess(error)))
       );
