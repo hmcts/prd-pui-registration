93b324ab59b9fe8dfdc6f90dffd2f94084faa702
Alan McCabe - Jones <alan@localhost.localdomain>
2019-02-05 16:15:05 +0000

    bad meta data fix

--- api/states/templates/organisationPba.ts
+++ api/states/templates/organisationPba.ts
@@ -1,52 +1,43 @@
 export default {
-    formValues: {
-        approveDraftConsent: 'no',
-        visitedPages: {
-            create: true,
+    idPrefix: 'tbc',
+    name: 'organisation-pba',
+    header: "What's your payment by account (PBA) number for your organisation?",
+    formGroupValidators: [],
+    groups: [
+        {
+            input: {
+                label: {
+                    text: 'PBA number 1 (optional)',
+                    classes: 'govuk-label--m',
+                },
+                control: 'PBAnumber1',
+                classes: 'govuk-!-width-two-thirds',
+            },
         },
-        notesForAdmin: '',
-    },
-    meta: {
-        idPrefix: 'tbc',
-        name: 'organisation-pba',
-        header: "What's your payment by account (PBA) number for your organisation?",
-        formGroupValidators: [],
-        groups: [
-            {
-                input: {
-                    label: {
-                        text: 'PBA number 1 (optional)',
-                        classes: 'govuk-label--m',
-                    },
-                    control: 'PBAnumber1',
-                    classes: 'govuk-!-width-two-thirds',
+        {
+            input: {
+                label: {
+                    text: 'PBA number 1 (optional)',
+                    classes: 'govuk-label--m',
                 },
+                control: 'PBAnumber2',
+                classes: 'govuk-!-width-two-thirds',
             },
-            {
-                input: {
-                    label: {
-                        text: 'PBA number 1 (optional)',
-                        classes: 'govuk-label--m',
-                    },
-                    control: 'PBAnumber2',
-                    classes: 'govuk-!-width-two-thirds',
-                },
+        },
+        {
+            hiddenInput: {
+                control: 'nextUrl',
+                value: 'organisation-dx',
             },
-            {
-                hiddenInput: {
-                    control: 'nextUrl',
-                    value: 'DXreference',
-                },
+        },
+        {
+            button: {
+                control: 'createButton',
+                value: 'Continue',
+                type: 'submit',
+                classes: '',
+                onEvent: 'continue',
             },
-            {
-                button: {
-                    control: 'createButton',
-                    value: 'Continue',
-                    type: 'submit',
-                    classes: '',
-                    onEvent: 'continue',
-                },
-            },
-        ],
-    },
+        },
+    ],
 }
