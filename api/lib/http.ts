ac7544fb0278a98a790dc79d71a1ee0d128057f1
Alan <alan.work@teamsmog.com>
2019-06-04 14:01:01 +0100

    remove proxy from local

--- api/lib/http.ts
+++ api/lib/http.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosInstance } from 'axios'
-import { globalProxy } from '../local'
 import config from './config'
 
 import { errorInterceptor, requestInterceptor, successInterceptor } from './interceptors'
@@ -10,9 +9,3 @@ axios.defaults.headers.common['Content-Type'] = 'application/json'
 
 http.interceptors.request.use(requestInterceptor)
 http.interceptors.response.use(successInterceptor, errorInterceptor)
-
-export function disableProxy() {
-    if (config.proxy) {
-        globalProxy.end()
-    }
-}
